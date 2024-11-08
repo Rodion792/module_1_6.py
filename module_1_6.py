47e8138bd0f7b114b47b3797e56cc3c2bea3145c
Rodion792 <duninrodion@gmail.com>
2024-11-08 17:55:25 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+my_dict = {'Artem': 83475643883, 'Lisa': 89493470479, 'Vitaly': 89543012673}
+print(my_dict)
+print(my_dict['Vitaly'])
+print(my_dict.get('Denis'))
+my_dict.update({'Sahsa': 8963104632,
+                'Alex': 8562205561})
+s = my_dict.pop('Vitaly')
+print(s)
+print(my_dict)
+
+my_set = { 1, 2, 76, 76, 98, 98, 'Alex', 'Denis', 'Denis', True}
+print(my_set)
+my_set.add(100)
+my_set.add(73)
+my_set.discard('Alex')
+print(my_set)
